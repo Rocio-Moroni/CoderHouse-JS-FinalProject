dc59ec05b8b3b1a84a7a059cf9b7e0e649d87142
ROCIO MORONI <rociomoroni@MacBook-Pro-de-ROCIO.local>
2021-11-24 00:16:25 -0300

    Entrega Final JS

--- app.js
+++ app.js
@@ -36,30 +36,29 @@ class Reserva {
         direccion,
         cp,
         formaDePago,
+        designacionDeCabaña,
         montoEstadia,
+        montoEstadiaUSD
     })
     {
-        this.nombre           = nombre;
-        this.apellido         = apellido;
-        this.numeroDeContacto = numeroDeContacto;
-        this.mail             = mail;
-        this.pais             = pais;
-        this.ciudad           = ciudad;
-        this.direccion        = direccion;
-        this.cp               = cp;
-        this.formaDePago      = formaDePago;
-        this.montoEstadia     = montoEstadia;
-
+        this.nombre              = nombre;
+        this.apellido            = apellido;
+        this.numeroDeContacto    = numeroDeContacto;
+        this.mail                = mail;
+        this.pais                = pais;
+        this.ciudad              = ciudad;
+        this.direccion           = direccion;
+        this.cp                  = cp;
+        this.formaDePago         = formaDePago;
+        this.designacionDeCabaña = designacionDeCabaña;
+        this.montoEstadia        = montoEstadia;
+        this.montoEstadiaUSD     = montoEstadiaUSD;
     }
 }
 
 // FUNCIONES.
 
-// Función para resetear los formularios completados.
-const resetFormDisponibilidad = () => {
-    document.getElementById('form__disponibilidad').reset();
-};
-
+// Función para resetear el formulario de RESERVA una vez realizado el pago.
 const resetFormReserva = () => {
     document.getElementById('form__reserva').reset();
 };
@@ -74,14 +73,14 @@ const extraerDatosFormDisponibilidad = () => {
     montoEstadiaUSDcalculado     = montoEstadia / precioDolarCompra;
 
     const nuevaDisponibilidad = new Disponibilidad ({
-        nombreCompleto: document.getElementById("form__disponibilidad-name").value,
-        cantidadAdultos: +document.getElementById("form__disponibilidad-adults").value,
-        cantidadMenores: +document.getElementById("form__disponibilidad-children").value,
-        totalAlojados: totalAlojadosCalculado,
-        cantidadNoches: +document.getElementById("form__disponibilidad-nights").value,
+        nombreCompleto:      document.getElementById("form__disponibilidad-name").value,
+        cantidadAdultos:     +document.getElementById("form__disponibilidad-adults").value,
+        cantidadMenores:     +document.getElementById("form__disponibilidad-children").value,
+        totalAlojados:       totalAlojadosCalculado,
+        cantidadNoches:      +document.getElementById("form__disponibilidad-nights").value,
         designacionDeCabaña: designacionDeCabañaOtorgada,
-        montoEstadia: montoEstadia,
-        montoEstadiaUSD: Math.round(montoEstadiaUSDcalculado)
+        montoEstadia:        montoEstadia,
+        montoEstadiaUSD:     Math.round(montoEstadiaUSDcalculado)
     })
     return nuevaDisponibilidad;
 };
@@ -93,21 +92,22 @@ const extraerDatosFormPago = () => {
     totalAlojadosCalculado       = totalAlojadosFuncion();
     designacionDeCabañaOtorgada  = designacionDeCabañaFuncion();
     formaDePagoElegido           = formaDePagoFuncion();
-    montoEstadia        = montoEstadiaFuncion();
+    montoEstadia                 = montoEstadiaFuncion();
     montoEstadiaUSDcalculado     = montoEstadia / precioDolarCompra;
 
     const nuevaReserva = new Reserva ({
-        nombre: document.getElementById("form__reserva-firstName").value,
-        apellido: document.getElementById("form__reserva-lastName").value,
-        numeroDeContacto: document.getElementById("form__reserva-phone").value,
-        mail: document.getElementById("form__reserva-email").value,
-        pais: document.getElementById("form__reserva-country").value,
-        ciudad: document.getElementById("form__reserva-city").value,
-        direccion: document.getElementById("form__reserva-address").value,
-        cp: document.getElementById("form__reserva-postalCode").value,
-        formaDePago: formaDePagoElegido,
-        montoEstadia: montoEstadia,
-        montoEstadiaUSD: Math.round(montoEstadiaUSDcalculado)
+        nombre:              document.getElementById("form__reserva-firstName").value,
+        apellido:            document.getElementById("form__reserva-lastName").value,
+        numeroDeContacto:    document.getElementById("form__reserva-phone").value,
+        mail:                document.getElementById("form__reserva-email").value,
+        pais:                document.getElementById("form__reserva-country").value,
+        ciudad:              document.getElementById("form__reserva-city").value,
+        direccion:           document.getElementById("form__reserva-address").value,
+        cp:                  document.getElementById("form__reserva-postalCode").value,
+        formaDePago:         formaDePagoElegido,
+        designacionDeCabaña: designacionDeCabañaOtorgada,
+        montoEstadia:        montoEstadia,
+        montoEstadiaUSD:     Math.round(montoEstadiaUSDcalculado)
     })
     return nuevaReserva;
 };
@@ -201,22 +201,22 @@ const dolarBlueFuncion = () => {
 // Función "formaDePago()" (Dependiendo de si se eligió pagar con tarjeta de crédito o con transferencia bancaria, el valor final de "formaDePago" será la opción elegida por el usuario).
 const formaDePagoFuncion = () => {
 
-    if (form__reserva-credit.checked == true) {
-        const formaDePago = document.getElementById("form__reserva-credit").value;
+    if (form__credit.checked == true) {
+        const formaDePago = document.getElementById("form__credit").value;
         return formaDePago;
-    } else if (form__reserva-wire.checked == true) {
-        const formaDePago = document.getElementById("form__reserva-wire").value;
+    } else if (form__wire.checked == true) {
+        const formaDePago = document.getElementById("form__wire").value;
         return formaDePago;
     }
 };
 
 // Función Shortcut Show (Si se elige pagar con tarjeta de crédito, aparecerá un nuevo "form" que mostrará las opciones de cantidad de cuotas sin interés que el huesped puede elegir para realizar su pago: 3, 6 y 12 cuotas).
-$("#form__reserva-credit").click (() => {
+$("#form__credit").click (() => {
     $(".form__credit-cuotas").show();
 });
 
 // Función Shortcut Hide (Si se elige como forma de pago transferencia bancaria, el "form" de cuotas se oculta).
-$("#form__reserva-wire").click (() => {
+$("#form__wire").click (() => {
     $(".form__credit-cuotas").hide();
 });
 
@@ -259,15 +259,14 @@ const guardarLocalStorageFormReserva = () => {
 // 2) "resetFormDisponibilidad()".
 // 3) "mostrarSeccionDisponibilidad()".
 // 4) "mostrarMensajeDisponibilidad()".
-document.getElementById('btn__availability').addEventListener('click', () => {guardarLocalStorageFormDisponibilidad(), resetFormDisponibilidad()});
+document.getElementById('btn__availability').addEventListener('click', () => {guardarLocalStorageFormDisponibilidad()});
 
 // Botón ejecuta:
 // 1) "guardarLocalStoragePago()".
 // 2) "resetFormPago()".
-window.addEventListener('DOMContentLoaded', document.getElementById('btn__book').addEventListener('click', () => {guardarLocalStorageFormReserva()}) )
-;
+
+document.getElementById('btn__book').addEventListener('click', () => {guardarLocalStorageFormReserva(), resetFormReserva()})
 
 // Botón ejecuta:
 // 1) "resetFormPago()".
-//document.getElementById('btn__resetFormReserva').addEventListener('click', () => {resetFormReserva()});
-
+document.getElementById('btn__resetFormReserva').addEventListener('click', () => {resetFormReserva()});
